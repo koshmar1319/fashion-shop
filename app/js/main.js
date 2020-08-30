45e0c530a1b1742b372001ed9f5f099ec488157c
Kirill <koshmar1319@mail.ru>
2020-08-30 20:32:06 +0300

    added footer plus adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -40,4 +40,16 @@ $(document).ready(function(){
     }
   });
 
+  const iconSearch = document.querySelector('.icons__search');
+  const body = document.querySelector('body');
+
+  iconSearch.addEventListener('click', function(event){
+    event.stopPropagation();
+    this.classList.add('icons__search--active');
+  });
+
+  body.addEventListener('click', function(event){
+    iconSearch.classList.remove('icons__search--active');
+  });
+
 });
